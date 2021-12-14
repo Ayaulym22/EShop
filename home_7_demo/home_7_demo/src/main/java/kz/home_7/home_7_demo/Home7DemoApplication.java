c2c873b39ac1f164890787d5f1c7225b4ef7dcc9
Ayaulym22 <73275629+Ayaulym22@users.noreply.github.com>
2021-12-15 02:20:10 +0600

    New
    information

---
+++ home_7_demo/home_7_demo/src/main/java/kz/home_7/home_7_demo/Home7DemoApplication.java
@@ -0,0 +1,13 @@
+package kz.home_7.home_7_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home7DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home7DemoApplication.class, args);
+	}
+
+}
---
+++ home_7_demo/home_7_demo/src/test/java/kz/home_7/home_7_demo/Home7DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.home_7.home_7_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home7DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
